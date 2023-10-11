8dd5774c778de5a0db7e3bf38a380fc423d1fb6f
ST-AR19 <1472699654@qq.com>
2023-10-11 20:30:44 +0800

    2023/10/11 第二道题

---
+++ 283.移动零.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=cpp
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int fast = 0;
+        int slow = 0;
+        for(; fast < nums.size(); fast++){
+            if(nums[fast] != 0){
+                nums[slow++] = nums[fast];
+            }
+        }
+        for(; slow < nums.size(); slow++){
+            nums[slow] = 0;
+        }
+    }
+};
+// @lc code=end
+
