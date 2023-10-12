b049d3b1f03776e93632ac2a49044bb814f8db27
ST-AR19 <1472699654@qq.com>
2023-10-12 21:56:22 +0800

    2023/10/12 第二道题

--- 904.水果成篮.cpp
+++ 904.水果成篮.cpp
@@ -11,15 +11,16 @@ public:
         int first = 0,second = 0;
         int result = 0;
         for(int end = 0;end < fruits.size();end++){
-            if(fruits[end] != fruits[first] && fruits[first] == fruits[second]){
+            if(fruits[end] != fruits[first] 
+                && fruits[first] == fruits[second]){//只有一个种类的时候加入第二个种类
                 second = end;
             }else if(fruits[end] != fruits[first] 
                     && fruits[end] != fruits[second]
-                    && fruits[first] != fruits[second]){
+                    && fruits[first] != fruits[second]){//出现第三个种类的情况
                         first = second;
                         second = end;
                         int temp = first;
-                        for(; temp < second; temp++){
+                        for(; temp < second; temp++){//确保当前窗口只有两个元素
                             if(fruits[temp] != fruits[first]
                                 && fruits[temp] != fruits[second]){
                                     first = temp;
