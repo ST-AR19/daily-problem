207e75a6676d3a3950f98aface615b6441505daf
ST-AR19 <1472699654@qq.com>
2023-11-25 15:06:46 +0800

    2023/11/25 第四道题

---
+++ 225.用队列实现栈.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=225 lang=cpp
+ *
+ * [225] 用队列实现栈
+ */
+
+// @lc code=start
+class MyStack {
+public:
+    queue<int> que;
+    MyStack() {
+
+    }
+    
+    void push(int x) {
+        que.push(x);
+    }
+    
+    int pop() {
+        int size=que.size();
+        size--;
+        while(size--){
+            que.push(que.front());
+            que.pop();
+        }
+        int result=que.front();
+        que.pop();
+        return result;
+    }
+    
+    int top() {
+        return que.back();
+    }
+    
+    bool empty() {
+        return que.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
+
