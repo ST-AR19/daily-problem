3cd7ba223417bfd129a4888f0cfd9cbe37a45608
ST-AR19 <1472699654@qq.com>
2023-11-25 14:57:39 +0800

    2023/11/25 第三道题

---
+++ 232.用栈实现队列.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=232 lang=cpp
+ *
+ * [232] 用栈实现队列
+ */
+
+// @lc code=start
+class MyQueue {
+public:
+    stack<int> sIn;
+    stack<int> sOut;
+    MyQueue() {
+
+    }
+    
+    void push(int x) {
+        sIn.push(x);
+    }
+    
+    int pop() {
+        if(sOut.empty()){
+            while(!sIn.empty()){
+                sOut.push(sIn.top());
+                sIn.pop();
+            }
+        }
+        int result = sOut.top();
+        sOut.pop();
+        return result;
+    }
+    
+    int peek() {
+        int res = this->pop();
+        sOut.push(res);
+        return res;
+    }
+    
+    bool empty() {
+        return sIn.empty() && sOut.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
+
