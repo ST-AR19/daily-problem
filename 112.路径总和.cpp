40b43e60779fac4f5ad37f98c08188b07fad4562
ST-AR19 <1472699654@qq.com>
2024-03-30 17:39:10 +0800

    2024/3/30 第十道题

---
+++ 112.路径总和.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=cpp
+ *
+ * [112] 路径总和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool judge(TreeNode* cur, int sum, int target, bool& result){
+        sum += cur->val;
+        if(cur->left) judge(cur->left, sum, target, result);
+        if(cur->right) judge(cur->right, sum, target, result);
+        if(cur->left == NULL && cur->right == NULL && sum == target) result = true;
+        return result;
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == NULL) return false;
+        int sum=0;
+        bool result = false;
+        judge(root, sum, targetSum, result);
+        return result;
+    }
+};
+// @lc code=end
+
