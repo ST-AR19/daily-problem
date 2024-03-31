dbbbca713fe5d98146893a5f876f71b674abe327
ST-AR19 <1472699654@qq.com>
2024-03-31 15:23:57 +0800

    2024/3/31 第五道题

---
+++ 700.二叉搜索树中的搜索.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=700 lang=cpp
+ *
+ * [700] 二叉搜索树中的搜索
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL) return NULL;
+        if(val > root->val) return searchBST(root->right, val);
+        else if(val < root->val) return searchBST(root->left, val);
+        else return root;
+    }
+};
+// @lc code=end
+
