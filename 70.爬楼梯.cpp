1d66eba708f55bda86d72aa804e629d17cf31b1e
ST-AR19 <1472699654@qq.com>
2024-04-27 14:22:13 +0800

    2024/4/27 增加了一个进阶问题的解答

--- 70.爬楼梯.cpp
+++ 70.爬楼梯.cpp
@@ -17,5 +17,23 @@ public:
         return dp[n-1];
     }
 };
+//进阶问题
+//你可以爬至多m (1 <= m < n)个台阶，输入数据包含两个正整数，
+/*#include <iostream>;
+#include <vector>;
+using namespace std;
+int main(){
+    int n, m;
+    while(cin>>n>>m){
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j <= m; j++){
+                if(i > j) dp[i] += dp[i-j];
+            }
+        }
+        cout<<dp[n]<<endl;
+    }
+}*/
 // @lc code=end
 
