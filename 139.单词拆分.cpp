e6f448d1b6d78ad92735ab87d581b984fa6b5502
ST-AR19 <1472699654@qq.com>
2024-05-07 23:36:55 +0800

    更新了这道题的回溯做法

--- 139.单词拆分.cpp
+++ 139.单词拆分.cpp
@@ -6,6 +6,35 @@
 
 // @lc code=start
 class Solution {
+//回溯算法
+/*private:
+    bool backtracking (const string& s,
+            const unordered_set<string>& wordSet,
+            vector<bool>& memory,
+            int startIndex) {
+        //回溯终点
+        if (startIndex >= s.size()) {
+            return true;
+        }
+        // 如果memory[startIndex]不是初始值了，直接使用memory[startIndex]的结果
+        if (!memory[startIndex]) return memory[startIndex];
+        for (int i = startIndex; i < s.size(); i++) {
+            string word = s.substr(startIndex, i - startIndex + 1);
+            if (wordSet.find(word) != wordSet.end() && backtracking(s, wordSet, memory, i + 1)) {
+                return true;
+            }
+        }
+        memory[startIndex] = false; // 记录以startIndex开始的子串是不可以被拆分的
+        return false;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        //剪枝，记录中间结果，减少时间消耗
+        vector<bool> memory(s.size(), 1); // -1 表示初始化状态
+        return backtracking(s, wordSet, memory, 0);
+    }
+*/
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
