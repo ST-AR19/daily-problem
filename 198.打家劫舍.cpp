42b641cfbc1dcb9cc8df8a8f3d5040908477c056
ST-AR19 <1472699654@qq.com>
2024-05-12 10:49:25 +0800

    经典dp问题，注意数组初始化和递推公式

---
+++ 198.打家劫舍.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=cpp
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        //根据初始化排除特殊情况
+        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 0) return 0;
+
+        vector<int> dp(nums.size(), 0);
+        vector<bool> flag(nums.size(), false);
+        //初始化
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i = 2; i < nums.size(); i++){
+            //dp公式
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1]);
+        }
+        return  dp[nums.size()-1];
+    }
+};
+// @lc code=end
+
