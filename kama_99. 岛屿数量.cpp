572cb56648ac3299af141e580cd65d795ef36fa9
ST-AR19 <1472699654@qq.com>
2024-06-26 12:39:21 +0800

    更新了广搜方法

--- kama_99. 岛屿数量.cpp
+++ kama_99. 岛屿数量.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
-void DFS(int n, int m, vector<vector<int>> board, vector<vector<bool>> &visited, int curX, int curY) {
+/*void DFS(int n, int m, vector<vector<int>> board, vector<vector<bool>> &visited, int curX, int curY) {
     for(int i = 0; i < 4; i++) {
         int nextX = curX + dir[i][0];
         int nextY = curY + dir[i][1];
@@ -15,7 +16,28 @@ void DFS(int n, int m, vector<vector<int>> board, vector<vector<bool>> &visited,
         }
     }
     return;
+}*/
+void BFS(int n, int m, vector<vector<int>> board, vector<vector<bool>> &visited, int curX, int curY) {
+    queue<pair<int, int>> que;
+    que.push({curX, curY});
+    visited[curX][curY] = true;
+    while(!que.empty()) {
+        pair<int, int> cur=que.front();
+        que.pop();
+        int x = cur.first;
+        int y = cur.second;
+        for(int i = 0; i < 4; i++) {
+            int nextX = x + dir[i][0];
+            int nextY = y + dir[i][1];
+            if(nextX < 0 || nextX >= n || nextY < 0 || nextY >= m) continue;
+            if(!visited[nextX][nextY] && board[nextX][nextY]) {
+                que.push({nextX, nextY});
+                visited[nextX][nextY] = true;
+            }
+        }
+    }
 }
+    
 
 int main() {
     int n,m;
@@ -35,7 +57,8 @@ int main() {
             if(!visited[i][j] && board[i][j]) {
                 result++;
                 visited[i][j]=true;
-                DFS(n, m, board, visited, i, j);
+                //DFS(n, m, board, visited, i, j);
+                BFS(n, m, board, visited, i, j);
                 //cout<<i<<" "<<j<<endl;
             }
         }
